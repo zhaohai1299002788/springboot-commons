6a0c6b9a1fb6bc562576ef68716cb56c85443db9
zhaohai1299002788 <zhaohaihzb@126.com>
2018-10-13 13:47:29 +0800

    过时类

--- src/main/java/com/github/zhaohzi1299002788/App.java
+++ src/main/java/com/github/zhaohzi1299002788/App.java
@@ -4,6 +4,7 @@ package com.github.zhaohzi1299002788;
  * Hello world!
  *
  */
+@Deprecated
 public class App 
 {
     public static void main( String[] args )
--- src/main/java/com/github/zhaohzi1299002788/DataUtils.java
+++ src/main/java/com/github/zhaohzi1299002788/DataUtils.java
@@ -2,6 +2,7 @@ package com.github.zhaohzi1299002788;
 
 import java.util.Date;
 
+@Deprecated
 public class DataUtils {
 
     public Date getDate() {
